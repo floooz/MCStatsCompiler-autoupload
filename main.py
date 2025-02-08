6452adf75288fa8ed94a6413ed6afc64f49584c1
Elric02 <elric.milet@gmail.com>
2025-02-08 18:56:12 +0100

    Import data files

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import json
+import os
+
+# 1. Import each json file and store it
+# 2. Import a names.csv config file with translations from UUID to username
+# 3. Based on a category and sub-category of stats, return the leaderboard 
+
+for filename in os.listdir('stats'):
+    file = open('stats/' + filename)
+    json_string = json.load(file)
+    #print(json_string)
