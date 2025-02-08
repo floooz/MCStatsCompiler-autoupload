3dead63ecb91bd8427bc117c773354ba9c0b9db7
Elric02 <elric.milet@gmail.com>
2025-02-08 20:00:13 +0100

    Now using Pandas

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import json
 import os
+import pandas as pd
 
 # 1. Import each json file and store it
 # 2. Import a names.csv config file with translations from UUID to username
 # 3. Based on a category and sub-category of stats, return the leaderboard 
 
+df = pd.DataFrame()
+
 for filename in os.listdir('stats'):
+    print("Now processing", filename)
     file = open('stats/' + filename)
-    json_string = json.load(file)
-    #print(json_string)
+    data = json.load(file)
+    # Import the JSON to a Pandas DF
+    temp_df = pd.json_normalize(data, meta_prefix=True)
+    temp_df = temp_df.transpose().iloc[1:].rename({0: filename}, axis=1)
+    # Split the index (stats.blabla.blabla) into 3 indexes (stats, blabla, blabla)
+    temp_df.index = temp_df.index.str.split('.', expand=True)
+    if df.empty:
+        df = temp_df
+    else:
+        df = df.join(temp_df, how="right")
+    df.to_csv('temp.csv')
+    print(df)
