85a1b754e4e4aa1d9454efdd06353aa4ba62c23b
Elric02 <elric.milet@gmail.com>
2025-02-14 02:51:28 +0100

    Most pokemons caught leaderboard

--- cobblemon_module/cobblemon.py
+++ cobblemon_module/cobblemon.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 import configparser
+import openpyxl
+import datetime
 
 
 # To get: table with columns for players and rows for pokemons
@@ -42,6 +44,22 @@ def loadData(csvtoggle, csvpath):
     return df
 
 
+def most_pokemons_leaderboard(df):
+    # Load the Excel file
+    file_path = "output.xlsx"
+    sheet_name = "leaderboard1"
+    wb = openpyxl.load_workbook(file_path)
+    ws = wb[sheet_name]
+    i = 0
+    for index, row in df.iterrows():
+        ws.cell(row=i+3, column=3, value=index)
+        ws.cell(row=i+3, column=4, value=row[0])
+        i += 1
+    now = datetime.datetime.now()
+    ws.cell(row=13, column=2, value="Dernière update le "+now.strftime("%d.%m.%y à %H:%M"))
+    wb.save(file_path)
+
+
 # Read config
 config = configparser.ConfigParser()
 config.read('cobblemon_config.ini')
@@ -52,3 +70,9 @@ count_df = df.drop(['caughtTimestamp', 'discoveredTimestamp', 'isShiny'], level=
 print(count_df)
 count_df['times_caught'] = count_df.apply(lambda row: (row == "CAUGHT").sum(), axis=1)
 print(count_df['times_caught'].sort_values().to_string())
+count_df.drop('times_caught', axis=1, inplace=True)
+player_sum = pd.DataFrame((count_df == "CAUGHT").sum().sort_values())
+player_sum['index'] = range(39, 0, -1)
+player_sum = player_sum.iloc[::-1]
+print(player_sum)
+most_pokemons_leaderboard(player_sum.iloc[0:10])
