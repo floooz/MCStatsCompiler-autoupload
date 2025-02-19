9b8862dc47658ff2a7db7cad646fa6ec1cf4774c
Elric02 <elric.milet@gmail.com>
2025-02-19 18:31:34 +0100

    Use UUID if no username was provided

--- cobblemon_module/cobblemon.py
+++ cobblemon_module/cobblemon.py
@@ -42,6 +42,9 @@ def loadData(csvtoggle, csvpath, useftp, ftpserver, ftppath):
                 
                 temp_df = pd.json_normalize(data, meta_prefix=True)
                 temp_name = names.loc[names['uuid'] == filename[:-5]]['name']
+                if temp_name.empty:
+                    print("No username found for UUID", filename[:-5], " in names.csv, using UUID for this player instead.")
+                    temp_name = filename[:-5]
                 temp_df = temp_df.transpose().iloc[1:].rename({0: temp_name.iloc[0]}, axis=1)
                 
                 if not temp_df.empty:
@@ -69,7 +72,13 @@ def loadData(csvtoggle, csvpath, useftp, ftpserver, ftppath):
                 # Import the JSON to a Pandas DF
                 temp_df = pd.json_normalize(data, meta_prefix=True)
                 temp_name = names.loc[names['uuid'] == filename[:-5]]['name']
-                temp_df = temp_df.transpose().iloc[1:].rename({0: temp_name.iloc[0]}, axis=1)
+                temp_df = temp_df.transpose().iloc[1:]
+                if temp_name.empty:
+                    print("No username found for UUID", filename[:-5], " in names.csv, using UUID for this player instead.")
+                    temp_name = filename[:-5]
+                    temp_df = temp_df.rename({0: temp_name}, axis=1)
+                else:
+                    temp_df = temp_df.rename({0: temp_name.iloc[0]}, axis=1)
                 # Split the index (stats.blabla.blabla) into 3 indexes (stats, blabla, blabla)
                 if not temp_df.empty:
                     temp_df.index = temp_df.index.str.split('.', expand=True)
